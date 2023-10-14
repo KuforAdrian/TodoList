97ef3ed6e9b9f55b40538d660935a97502f756ba
KuforAdrian <adrianteintsem@gmail.com>
2023-10-14 11:17:10 +0100

    Up to date

---
+++ src/com/adrian/todolist/DialogController.java
@@ -0,0 +1,34 @@
+package com.adrian.todolist;
+
+import com.adrian.todolist.datamodel.TodoData;
+import com.adrian.todolist.datamodel.TodoItem;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+
+/**
+ * Created by adria on 5/29/2023.
+ */
+public class DialogController {
+    @FXML
+    private TextField shortDescriptionField;
+
+    @FXML
+    private TextArea detailsArea;
+
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults(){
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoItem newItem = new TodoItem(shortDescription, details, deadlineValue);
+        TodoData.getInstance().addTodoItem(newItem);
+        return newItem;
+    }
+}
